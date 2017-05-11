09a3ed9241346f3918e35d8f68bbc8114482f222
Neucrack <CZD666666@gmail.com>
2017-05-11 17:55:31 +0800

    add hardware PCB

---
+++ hardware/Clean.bat
@@ -0,0 +1,22 @@
+del *.SchDocPreview
+del *.HarnessPreview
+del *.PcbDocPreview
+del *.TopPreview
+del *.PrjPcbStructure
+del *.PcbDoc.htm
+del *.apr
+del *.DRR
+del *.EXTREP
+del *.LDP
+del *.REP
+del *.RUL
+del *.APR_LIB
+del *.Dat
+del "Status Report.Txt"
+del *.Dat
+del *.log
+rd ProjectOutputs /s/q
+rd History /s/q
+rd __Previews /s/q
+for /D %%a in ("Project Logs for*") do rd "%%a" /s/q
+for /D %%a in ("Project Outputs for*") do rd "%%a" /s/q
